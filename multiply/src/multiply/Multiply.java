7fc7e5ed785013ba41d97fe1dc46a4a58bff2ec1
choprachirag002 <75828364+choprachirag002@users.noreply.github.com>
2021-02-10 13:23:58 +0530

    sum
    sum

--- multiply/src/multiply/Multiply.java
+++ multiply/src/multiply/Multiply.java
@@ -3,9 +3,9 @@ package multiply;
 public class Multiply {
 	public static void main(String[]args)
 	{
-		int a=5;
-		int b=7;
-		System.out.println(a*b);
+		int i=5;
+		int j=7;
+		System.out.println(i+j);
 	}
 
 }
